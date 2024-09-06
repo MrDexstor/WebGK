81225749cb7596a2e28fa38ee0168cd0c4d03650
MrDexstor <zenfen_tx@mail.ru>
2024-09-06 23:53:53 +0300

    Меню

--- Lib/UI.py
+++ Lib/UI.py
@@ -1,10 +1,19 @@
 from django import shortcuts
+from Core.models import Menu
 
-
-def render(request, template_name, title='', context={}):
+def redirect(*args, **kwargs):
+    return shortcuts.redirect(*args, **kwargs)
+    
+    
+def render(request, template_name, title='', context={}, guest_mode=False):
+    if guest_mode is False:
+        if request.user.is_authenticated:
+            context['GKRightMenu'] = Menu.objects.all()
+        else:
+            return redirect('main')
+    
+    
     context['title'] = title
     return shortcuts.render(request, template_name, context)
     
     
-def redirect(*args, **kwargs):
-    return shortcuts.redirect(*args, **kwargs)
